0d66f3998cb7c3996b243a0e505d1ba5e3299cd0
Lucas Tenório dos Santos <tenori_o@outlook.com>
2020-04-26 10:51:27 -0300

    Jogada especial Promocao

--- xadrez-console/xadrez/PartidaXadrez.cs
+++ xadrez-console/xadrez/PartidaXadrez.cs
@@ -298,6 +298,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em cheque");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #Jogada Especial - Promoção
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RemoverPeca(destino);
+                    _pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.AdicionarPeca(dama, destino);
+                    _pecas.Add(dama);
+                }
+            }
+
+
             if (EstaEmCheque(Adversaria(JogadorAtual)))  {
                 EmXeque = true;
             }
@@ -314,8 +330,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #JogadaEspecial En Passant
             if (p is Peao && destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)
